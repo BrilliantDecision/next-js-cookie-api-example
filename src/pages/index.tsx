75a9e909a1351a364cc4b19377973d0bd2e6d5c2
BrilliantDecision <alexmobilepost@gmail.com>
2025-05-04 22:03:17 +0300

    feat:main initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import { useState } from "react";
+import { getCookie, setCookie } from "cookie-js-api";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,6 +14,16 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  const [nameCookie, setNameCookie] = useState("");
+  const [valueCookie, setValueCookie] = useState("");
+  const [valueFromBrowser, setValueFromBrowser] = useState("");
+  const onSetCookie = () => {
+    setCookie(nameCookie, valueCookie);
+  };
+
+  const onGetCookie = () => {
+    setValueFromBrowser(getCookie(nameCookie));
+  };
   return (
     <>
       <Head>
@@ -23,94 +33,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div
-        className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
+        className={`${geistSans.variable} ${geistMono.variable}`}
       >
-        <main className={styles.main}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-          <ol>
-            <li>
-              Get started by editing <code>src/pages/index.tsx</code>.
-            </li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
-
-          <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className={styles.logo}
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-              Read our docs
-            </a>
-          </div>
-        </main>
-        <footer className={styles.footer}>
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
+        <p>Cookie name:</p>
+        <input type="text" onChange={(e) => setNameCookie(e.target.value)} />
+        <p>Cookie value:</p>
+        <input type="text" onChange={(e) => setValueCookie(e.target.value)} />
+        <button onClick={onSetCookie}>Set cookie</button>
+        <p>Cookie value from browser cookie api:</p>
+        <p>{valueFromBrowser}</p>
+        <button onClick={onGetCookie}>Get cookie</button>
       </div>
     </>
   );
